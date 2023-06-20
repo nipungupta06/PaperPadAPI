d6aa0623914192da47b3a73ce155c4a8bb7e6e77
Nipun <nipungupta593@gmail.com>
2023-06-20 19:00:57 +0530

    Connected to mongoDB

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const connectToMongo =require("./db")
 const express = require('express')
 const app = express()
+
 connectToMongo();
 
-app.get('/', (req, res) => {
-    res.send('hello world')
-  })
+app.use(express.json())
+app.use('/api/auth',require('./routes/auth'))
+// app.use('/api/notes',require('./routes/notes'))
 
 app.listen(3000,()=>{
     console.log("Listening to port 3000")
